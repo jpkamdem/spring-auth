8e69d4658afb9131fae4000bae24678411de33fc
jp kam <jpkamdem@proton.me>
2025-08-14 13:48:37 +0200

    init

---
+++ src/test/java/konnro/server/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package konnro.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
