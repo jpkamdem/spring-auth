18bca0e9549861b96c36dd957cbd40a2c1cb881f
jpkam <jpkamdem@proton.me>
2025-08-18 00:10:12 +0200

    refonte securityConfig, form login refusé, postman test, test

---
+++ src/main/java/konnro/server/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package konnro.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(request -> request
+            .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+            .anyRequest().authenticated())
+        // .formLogin(customizer -> customizer.disable())
+        .httpBasic(Customizer.withDefaults());
+    return http.build();
+  }
+}
