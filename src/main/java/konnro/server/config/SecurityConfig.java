077730f526e40f00096cf07c460210b52f5753a1
jpkam <jpkamdem@proton.me>
2025-08-18 21:21:59 +0200

    fix

--- src/main/java/konnro/server/config/SecurityConfig.java
+++ src/main/java/konnro/server/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package konnro.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -11,15 +12,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .csrf(csrf -> csrf.disable())
         .formLogin(form -> form.disable())
-        .httpBasic(Customizer.withDefaults())
+        .httpBasic(basic -> basic.disable())
         .authorizeHttpRequests(request -> request
-          .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
-          .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll())
+            .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+            .anyRequest().authenticated())
         .build();
   }
 }
